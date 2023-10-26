ab003335997432dfdd13435dd75958c8196b3a3d
Sumit Maurya <smithdashdash@gmail.com>
2023-10-26 15:48:12 +0530

    added toggle between browse and homepage

--- src/Pages/Movies.jsx
+++ src/Pages/Movies.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import Profile from '../assets/images/user-image.png'
 import List from '../Components/Movies/List';
+import { useNavigate } from 'react-router-dom';
 const Movies = () => {
   const movies = JSON.parse(window.localStorage.getItem("genres"));
+  const Navigate = useNavigate();
+  const handleClick = ()=>{
+    Navigate("/browse");
+  }
   return (
     <>
     <div style={{height:"100vh",width:"100vw",backgroundColor:"black",overflowX:"hidden"}}>
@@ -10,7 +15,9 @@ const Movies = () => {
           Super App
           <p style={{color:"white",fontFamily:"Roboto",fontSize:"2rem",fontWeight:"600"}}>Entertainment according to your choice</p>
         </div>
-        <img src={Profile} style={{position:"absolute",height:"5rem",width:"5rem",top:"3vh",right:"3vw",borderRadius:"50%"}} alt="user"/>
+        <img src={Profile} style={{position:"absolute",height:"5rem",width:"5rem",top:"3vh",right:"3vw",borderRadius:"50%",cursor:"pointer"}}
+        onClick={handleClick}
+        alt="user"/>
         <div style={{marginTop:"6vh",marginLeft:"4vw"}}>
         {movies.map((genre)=>
           <List key={genre} genre={genre}/>
